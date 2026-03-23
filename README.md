9eb93a9dc8657023c4f2b497408c09f016db5e4b
JCEEE <0xjceee@proton.me>
2026-03-23 13:41:19 +0000

    change meta tags

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ![cover](https://threejjjs-cdn.s3.eu-west-2.amazonaws.com/whatarewebuilding/wawb.jpg)
 
 Landing website for local web development company on behalf of threejjjs.
+
+> Dev note: crop `710` from both sides when processing mobile videos from 1920x1080 sources
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,22 +4,22 @@ export const load = ({ url }) => {
 	const baseMetaTags = Object.freeze({
 		title: 'wahatarewebuilding | a web design company',
 		titleTemplate: '%s | Meta Tags',
-		description: 'Build it | Make it | Keep it | Send it LOCAL',
+		description: 'Build it | Make it | Keep it | Send it LOCAL ...Design and Development for Business and Personal projects. Lets do websites and tech infrastructure together.',
 		canonical: new URL(url.pathname, url.origin).href,
 		openGraph: {
 			type: 'website',
 			url: new URL(url.pathname, url.origin).href,
 			locale: 'en_GB',
 			title: 'Walthamstow! Whatarewebuilding?',
-			description: 'Build it | Make it | Keep it | Send it LOCAL',
+			description: 'Build it | Make it | Keep it | Send it LOCAL ...Design and Development for Business and Personal projects. Lets do websites and tech infrastructure together.',
 			siteName: 'Whatarewebuilding',
 			images: [
 				{
-					url: 'https://threejjjs-cdn.s3.eu-west-2.amazonaws.com/whatarewebuilding/wawb.jpg',
+					url: 'https://threejjjs-cdn.s3.eu-west-2.amazonaws.com/whatarewebuilding/wawb-wide.jpg',
 					alt: 'Og Image Alt',
-					width: 800,
-					height: 600,
-					secureUrl: 'https://threejjjs-cdn.s3.eu-west-2.amazonaws.com/whatarewebuilding/wawb.jpg',
+					width: 744,
+					height: 386,
+					secureUrl: 'https://threejjjs-cdn.s3.eu-west-2.amazonaws.com/whatarewebuilding/wawb-wide.jpg',
 					type: 'image/jpeg'
 				}
 			]
